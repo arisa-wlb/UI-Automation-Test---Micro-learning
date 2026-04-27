c5a370df093ebbe33f9bfc1f10b3460c2057cb6a
Arisa <arisa@welovebug.biz>
2026-04-27 13:42:43 +0700

    [Added]: เพิ่ม test fucntion Login  ไม่สำเร็จ Email ไม่ถูกต้อง ในไฟล์ login.spec.ts

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -53,3 +53,31 @@ test('Login  ไม่สำเร็จ password ไม่ถูกต้อง
       });
       
 });
+
+test('Login  ไม่สำเร็จ Email ไม่ถูกต้อง' , async ({ page }) => {
+  await test.step('เข้าสู่หน้าเว็บไซต์' , async () => {
+    await page.goto('https://ui-sandbox-omega.vercel.app');
+  });
+
+  await test.step('ไปที่หน้า Login' , async () => {
+    await page.getByRole('link', { name: 'Login Form' }).click()
+  });
+  await test.step('กรอก Email เท่ากันกับ wronguser@company.com  และกรอก password เท่ากันกับ Test1234!' , async () => {
+    const emailInput = page.getByTestId('email-input')
+    await expect(emailInput).toBeVisible()
+    await emailInput.fill('wronguser@company.com')
+    await page.getByTestId('password-input').fill('Test1234!')
+  });
+
+  await test.step('กดปุ่ม Login' , async () => {
+    await page.getByTestId('login-btn').click()
+  });
+
+  await test.step('ตรวจสอบ popup และข้อความใน pop up' , async () => {
+    const toast = page.getByTestId('toast-msg')
+    await expect(toast).toBeVisible()
+    await expect(toast).toHaveText('Invalid email or password. Please try again.')
+  });
+  
+});
+
