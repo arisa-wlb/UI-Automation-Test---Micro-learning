3609e92e773fe4d11a5ca2b6104d8d8fa381f0bd
Arisa <arisa@welovebug.biz>
2026-04-27 15:15:06 +0700

    [Added]: เพิ่ม Test Step เข้าสู่หน้าเว็บไซต์ และ ไปหน้า Shipping Details ใน  tests/shippingForm.spec.ts

--- tests/shippingForm.spec.ts
+++ tests/shippingForm.spec.ts
@@ -1,6 +1,9 @@
 import { test, expect } from '@playwright/test';
 
 test('Confirm Shipping สำเร็จ' , async ({ page }) => {
+    await test.step('เข้าสู่หน้าเว็บไซต์ และ ไปหน้า Shipping Details' , async () => {
+        await page.goto('https://ui-sandbox-omega.vercel.app');
+        await page.getByRole('link', { name: 'Shipping Form' }).click()
+    });
 
-    
 });
