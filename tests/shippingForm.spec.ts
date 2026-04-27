1adccc4737a12eeb0eaacc608fa5e90214951ac2
Arisa <arisa@welovebug.biz>
2026-04-27 15:17:57 +0700

    [Added]: เพิ่ม Test Step กรอกข้อมูลส่วน Recipient ใน  tests/shippingForm.spec.ts

--- tests/shippingForm.spec.ts
+++ tests/shippingForm.spec.ts
@@ -5,5 +5,10 @@ test('Confirm Shipping สำเร็จ' , async ({ page }) => {
         await page.goto('https://ui-sandbox-omega.vercel.app');
         await page.getByRole('link', { name: 'Shipping Form' }).click()
     });
+    await test.step('กรอกข้อมูลส่วน Recipient' , async () => {
+        await page.getByTestId('shipping-form-first-name').fill('Somchai')
+        await page.getByTestId('shipping-form-last-name').fill('Jaidee')
+        await page.getByTestId('shipping-form-phone').fill('089-654-2124')
+    });
 
 });
