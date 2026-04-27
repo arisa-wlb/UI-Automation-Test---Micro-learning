64710d19776593e8300e259d51d68cf29cfb4d36
Arisa <arisa@welovebug.biz>
2026-04-27 10:47:52 +0700

    [Added]: เพิ่ม test step เข้าสู่หน้าเว็บไซต์ใน tests/login.spec.ts เพื่อเริ่มต้น flow ของ login test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,5 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test('Login สำเร็จ', async ({ page }) => {
+      await test.step('เข้าสู่หน้าเว็บไซต์' , async() => {
+        await page.goto('https://ui-sandbox-omega.vercel.app');
+      });
+      
+
+
 
 });
