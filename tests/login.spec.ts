493a9d43fe3385cd383d7ec6c439189cb192fe91
Arisa <arisa@welovebug.biz>
2026-04-27 10:51:54 +0700

    [Added]: เพิ่ม test step ไปที่หน้า Login ใน  tests/login.spec.ts

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,10 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('Login สำเร็จ', async ({ page }) => {
-      await test.step('เข้าสู่หน้าเว็บไซต์' , async() => {
+      await test.step('เข้าสู่หน้าเว็บไซต์' , async () => {
         await page.goto('https://ui-sandbox-omega.vercel.app');
       });
-      
+
+      await test.step('ไปที่หน้า Login' , async () => {
+        await page.getByRole('link', { name: 'Login Form' }).click()
+      })
+
 
 
 
