f067e7c5c59a759b8c87b5789dba010deb6a11d5
Arisa <arisa@welovebug.biz>
2026-04-28 16:00:03 +0700

    [Added]: เพิ่ม test step ไปหน้า Confirm Payment ของ Test function กรอกข้อมูล Payment และ Confirm Payment สำเร็จ  ใน ไฟล์ confirm-payment.spec.ts

--- tests/confilm-payment.spec.ts
+++ tests/confilm-payment.spec.ts
@@ -3,8 +3,12 @@ import { test, expect } from '@playwright/test';
 
 test('กรอกข้อมูล Payment และ Confirm Payment สำเร็จ', async ({ page }) => {
 
-    await test.step('เข้าสู่หน้าเว็บไซต์', async () => {
-        await page.goto('https://ui-sandbox-omega.vercel.app');
-      });
+  await test.step('เข้าสู่หน้าเว็บไซต์', async () => {
+    await page.goto('https://ui-sandbox-omega.vercel.app');
+  });
+
+  await test.step('ไปหน้า Confirm Payment', async () => {        
+    await page.getByRole('link', { name: 'Confirm Payment' }).click();      
+  });
         
 });
