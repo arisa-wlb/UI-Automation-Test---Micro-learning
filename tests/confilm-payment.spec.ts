2abf8c7d90756ec3121345c1cf9764edb225db9e
Arisa <arisa@welovebug.biz>
2026-04-28 15:58:33 +0700

    [Added]: เพิ่ม test step เข้าสู่หน้าเว็บไซต์ ของ Test function กรอกข้อมูล Payment และ Confirm Payment สำเร็จ  ใน ไฟล์ confirm-payment.spec.ts

--- tests/confilm-payment.spec.ts
+++ tests/confilm-payment.spec.ts
@@ -3,4 +3,8 @@ import { test, expect } from '@playwright/test';
 
 test('กรอกข้อมูล Payment และ Confirm Payment สำเร็จ', async ({ page }) => {
 
+    await test.step('เข้าสู่หน้าเว็บไซต์', async () => {
+        await page.goto('https://ui-sandbox-omega.vercel.app');
+      });
+        
 });
