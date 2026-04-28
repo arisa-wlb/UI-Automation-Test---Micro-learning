3434c9275b4d9612f9042e44e8a2065d1a4e003c
Arisa <arisa@welovebug.biz>
2026-04-28 09:27:23 +0700

    [Config]: แก้ไข baseURL ใน playwright.config.ts เพื่อเปลี่ยน environment สำหรับการรัน test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('Login สำเร็จ', async ({ page }) => {
       await test.step('เข้าสู่หน้าเว็บไซต์' , async () => {
-        await page.goto('https://ui-sandbox-omega.vercel.app');
+        await page.goto('/');
       });
 
       await test.step('ไปที่หน้า Login' , async () => {
