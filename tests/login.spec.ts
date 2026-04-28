0882852eb5a34eaa9d6b31b85169616370f80a52
Arisa <arisa@welovebug.biz>
2026-04-28 10:08:01 +0700

    [Edited]: แยก test step ใน tests/login.spec.ts หลายจุดเพื่อให้ step ชัดเจนและปรับปรุง readability ของ test flow

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -9,10 +9,14 @@ test('Login สำเร็จ', async ({ page }) => {
         await page.getByRole('link', { name: 'Login Form' }).click()
       });
 
-      await test.step('กรอก Email เท่ากันกับ user@company.com และกรอก password เท่ากันกับ Test1234!' , async () => {
+      await test.step('กรอก Email เท่ากันกับ user@company.com' , async () => {
         const emailInput = page.getByTestId('email-input')
         await expect(emailInput).toBeVisible()
         await emailInput.fill('user@company.com')
+    
+      });
+
+      await test.step('กรอก password เท่ากันกับ Test1234!' , async () => {
         await page.getByTestId('password-input').fill('Test1234!')
       });
       
@@ -20,8 +24,11 @@ test('Login สำเร็จ', async ({ page }) => {
         await page.keyboard.press('Enter');
       });
 
-      await test.step('ตรวจสอบ url และ ข้อความบนหน้าเว็บ' , async () => {
+      await test.step('ตรวจสอบ url เท่ากันกับ login/success ' , async () => {
         await expect(page).toHaveURL('https://ui-sandbox-omega.vercel.app/login/success');
+      });
+
+      await test.step('ตรวจสอบข้อความบนหน้าเว็บเมื่อ Login สำเร็จ แสดงข้อความ เท่ากันกับ Login successful You have signed in as user@company.com'  , async () => {
         await expect(page.getByTestId('success-heading')).toHaveText('Login successful')
         await expect(page.getByTestId('signin-user-message')).toHaveText('You have signed in as user@company.com')
       });
@@ -29,16 +36,20 @@ test('Login สำเร็จ', async ({ page }) => {
 
 test('Login  ไม่สำเร็จ password ไม่ถูกต้อง' , async ({ page }) => {
       await test.step('เข้าสู่หน้าเว็บไซต์' , async () => {
-        await page.goto('https://ui-sandbox-omega.vercel.app');
+        await page.goto('/');
       });
 
       await test.step('ไปที่หน้า Login' , async () => {
         await page.getByRole('link', { name: 'Login Form' }).click()
       });
-      await test.step('กรอก Email เท่ากันกับ user@company.com และกรอก password เท่ากันกับ Test1234!!!!!!' , async () => {
+
+      await test.step('กรอก Email เท่ากันกับ user@company.com' , async () => {
         const emailInput = page.getByTestId('email-input')
         await expect(emailInput).toBeVisible()
         await emailInput.fill('user@company.com')
+      });
+
+      await test.step('กรอก password เท่ากันกับ Test1234!!!!!!' , async () => {
         await page.getByTestId('password-input').fill('Test1234!!!!!!')
       });
 
@@ -46,7 +57,7 @@ test('Login  ไม่สำเร็จ password ไม่ถูกต้อง
         await page.getByTestId('login-btn').click()
       });
 
-      await test.step('ตรวจสอบ popup และข้อความใน pop up' , async () => {
+      await test.step('ตรวจสอบว่ามี popup แสดง และข้อความใน popup เท่ากันกับ Invalid email or password. Please try again.' , async () => {
         const toast = page.getByTestId('toast-msg')
         await expect(toast).toBeVisible()
         await expect(toast).toHaveText('Invalid email or password. Please try again.')
@@ -56,16 +67,20 @@ test('Login  ไม่สำเร็จ password ไม่ถูกต้อง
 
 test('Login  ไม่สำเร็จ Email ไม่ถูกต้อง' , async ({ page }) => {
   await test.step('เข้าสู่หน้าเว็บไซต์' , async () => {
-    await page.goto('https://ui-sandbox-omega.vercel.app');
+    await page.goto('/');
   });
 
   await test.step('ไปที่หน้า Login' , async () => {
     await page.getByRole('link', { name: 'Login Form' }).click()
   });
-  await test.step('กรอก Email เท่ากันกับ wronguser@company.com  และกรอก password เท่ากันกับ Test1234!' , async () => {
+
+  await test.step('กรอก Email เท่ากันกับ wronguser@company.com' , async () => {
     const emailInput = page.getByTestId('email-input')
     await expect(emailInput).toBeVisible()
     await emailInput.fill('wronguser@company.com')
+  });
+
+  await test.step('กรอก password เท่ากันกับ Test1234!' , async () => {
     await page.getByTestId('password-input').fill('Test1234!')
   });
 
@@ -73,17 +88,16 @@ test('Login  ไม่สำเร็จ Email ไม่ถูกต้อง' ,
     await page.getByTestId('login-btn').click()
   });
 
-  await test.step('ตรวจสอบ popup และข้อความใน pop up' , async () => {
+  await test.step('ตรวจสอบว่ามี popup แสดง และข้อความใน popup เท่ากันกับ Invalid email or password. Please try again.' , async () => {
     const toast = page.getByTestId('toast-msg')
     await expect(toast).toBeVisible()
     await expect(toast).toHaveText('Invalid email or password. Please try again.')
   });
-  
 });
 
 test('Login  ไม่สำเร็จ ไม่กรอก Email และ Password ' , async ({ page }) => {
   await test.step('เข้าสู่หน้าเว็บไซต์' , async () => {
-    await page.goto('https://ui-sandbox-omega.vercel.app');
+    await page.goto('/');
   });
 
   await test.step('ไปที่หน้า Login' , async () => {
@@ -94,9 +108,13 @@ test('Login  ไม่สำเร็จ ไม่กรอก Email และ P
     await page.getByTestId('login-btn').click()
   });
 
-  await test.step('ตรวจสอบ error ใต้ช่องกรอก Email และ Password' , async () => {
+  await test.step('ตรวจสอบ error ใต้ช่องกรอก Email เท่ากันกับ Please enter a valid email address. ' , async () => {
     await expect(page.getByTestId('email-error')).toHaveText('Please enter a valid email address.')
+  
+  });
+
+  await test.step('ตรวจสอบ error ใต้ช่องกรอก Password เท่ากันกับ Password is required.' , async () => {
     await expect(page.getByTestId('password-error')).toHaveText('Password is required.')
   });
-  
+
 });
